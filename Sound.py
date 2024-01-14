6735d57baeadd32fa847ffac61bb08851a446f35
khajan_bhatt <140534285+Khajan38@users.noreply.github.com>
2024-01-14 10:03:54 +0530

    Add files via upload

---
+++ Sound.py
@@ -0,0 +1,14 @@
+from playsound import playsound
+def ADERROR():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Sorry, Wrong Password.mp3')
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Access_Denied_Preview.mp3')
+def WELCOME():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Welcome to Great...mp3')
+def INPUT():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Invalid Input.mp3')
+def CONTQUIT():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Continue and quit.mp3')
+def THANKS():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Thanks.mp3')
+def FEEDBACK():
+     playsound('D:\Python IDLE\Lib\site-packages\Railway\Sounds\Feedback.mp3')
